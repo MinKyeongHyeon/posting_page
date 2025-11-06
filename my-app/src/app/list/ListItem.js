06625c394d98f095f37ddc8ecfae740127a65472
MinKyeongHyeon <28599@naver.com>
2025-11-06 17:31:06 +0900

    fix/mongodb 불러오는 방법 최신화

--- my-app/src/app/list/ListItem.js
+++ my-app/src/app/list/ListItem.js
@@ -25,6 +25,6 @@ export default function ListItem(props) {
           </span>
         </div>
       ))}
-    </div>
+    </div> 
   );
 }
--- my-app/utils/database.js
+++ my-app/utils/database.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
 const url =
   "mongodb+srv://manemin:rudgus0712@mymongocluster.lmedt4k.mongodb.net/?retryWrites=true&w=majority&appName=MyMongoCluster";
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
 let connectDB;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
+    global._mongo = new MongoClient(url).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+  connectDB = new MongoClient(url).connect();
 }
 export { connectDB };
